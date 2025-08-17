8fca67187d1fde41583a2e8468312c613024b949
Md Raza <mdraza8397@gmail.com>
2025-08-18 00:12:44 +0530

    students insert and read functionality added

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Student;
+
+class StudentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $students = Student::all();
+        return view('students.index', compact('students'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('students.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $credentials = $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:students',
+            'age' => 'required|integer',
+        ]);
+        Student::create($request->all());
+        return view('students.index');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ resources/views/students/index.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>AA</title>
+    @vite(['resources/css/app.css'])
+</head>
+
+<body class="bg-gray-200">
+    <div class="flex">
+        <table border="2" cellpadding="5" class="text-black">
+            <tr>
+                <th>Id</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Age</th>
+                <th>Created At</th>
+            </tr>
+            @foreach ($students as $student)
+                <tr>
+                    <td>{{ $student->id }}</td>
+                    <td>{{ $student->name }}</td>
+                    <td>{{ $student->email }}</td>
+                    <td>{{ $student->age }}</td>
+                    <td>{{ $student->created_at == null ? 'Na' : $student->created_at }}</td>
+                </tr>
+            @endforeach
+        </table>
+    </div>
+</body>
+
+</html>
